671a7a306943f76dd4cf23bfd42cddb5e9d32392
Thomas GRAPPIN <tgrappin1986@gmail.com>
2024-09-15 21:17:13 +0200

    init

--- src/main/java/fr/softway/autodiagnostic/service/AutoDiagnosticService.java
+++ src/main/java/fr/softway/autodiagnostic/service/AutoDiagnosticService.java
@@ -42,7 +42,7 @@ public class AutoDiagnosticService {
     /**
      *
      * @param indexSante index santé du patient
-     * @return le ou les unités médicales à visiter selon le diagnostic
+     * @return une chaîne décrivant l'unité médicale ou les unités médicales à visiter selon le diagnostic
      */
     public String traiterDonnees(Long idPatient, int indexSante) {
         List<String> unitesMedicales = new ArrayList<>();
@@ -52,8 +52,8 @@ public class AutoDiagnosticService {
             unitesMedicales.add(UniteMedicale.TRAUMATOLOGIE.name());
         String unitesMedicalesAVisiter = String.join(", ", unitesMedicales);
         log.info("unitesMedicales: {}", unitesMedicalesAVisiter);
-        Optional<AutoDiagnosticEntity> autoDiagnostic = autoDiagnosticRepository.findByIdPatient(idPatient);
-        autoDiagnostic.ifPresent(diagnostic -> {
+        Optional<AutoDiagnosticEntity> autoDiagnosticOptional = autoDiagnosticRepository.findByIdPatient(idPatient);
+        autoDiagnosticOptional.ifPresent(diagnostic -> {
             diagnostic.setUnitesAVisiter(unitesMedicalesAVisiter);
             autoDiagnosticRepository.save(diagnostic);
         });
